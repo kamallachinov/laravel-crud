fea5a885aa41cf873d33e115c3d3564ed7795e39
Kamal Lachinov <kamallachinov5@gmail.com>
2025-01-10 17:11:36 +0400

    learn about input validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,15 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public  function register()
+    public  function register(Request $request)
     {
+        $incomingField = $request->validate([
+            'username' => ['required', 'min:3', 'max:10'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:6', 'max:255'],
+        ]);
+
+        dd($incomingField);
         return "KAMALLLLLL";
     }
 }
